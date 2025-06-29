765397cceaecf858c273be4d387ebfce2febe5ef
skidder <154992220+wxzzgs@users.noreply.github.com>
2025-06-28 21:39:50 -0500

    Delete Methampton.exe (Ransomeware) directory

--- Methampton.exe (Ransomeware)/Run at your own risk + credits.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	cout << "Run at your own risk!" << endl;
-	cout << "made by OceanEmpireRblx or notoceandisasters on discord!." << endl;
-}
